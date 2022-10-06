b7d0203da9d83f9dbe1aa671d3939531dfe5e0fc
nilesh <avnp16@gmail.com>
2022-10-06 14:32:40 +0530

    06-10-2022

--- Creative/13_Array/13.10.c
+++ Creative/13_Array/13.10.c
@@ -1,1 +1,38 @@
 //13.1	WAP to get the array and search the element from it
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n,s, i;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (i = 0; i < n; i++)
+    {
+        printf("a[%d]", i);
+        scanf("%d", &a[i]);
+    }
+
+   printf("Enter the item to be searched: ");
+   scanf("%d", &s);
+ 
+   //Research starts from the index 0
+   i = 0;
+   while (i < n && s != a[i]) {
+      i++;
+   }
+ 
+   if (i < n) {
+      printf("The element is found in the position = %d\n", i + 1);
+            printf("The element is found in the index number = %d\n", i);
+
+   } else {
+      printf("Element not found!");
+   }
+
+    return 0;
+}
--- Creative/13_Array/13.7.c
+++ Creative/13_Array/13.7.c
@@ -1,1 +1,28 @@
-//13.7	WAP Sum of the elements of the array
+// 13.7	WAP Sum of the elements of the array
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int sum = 0;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (i = 0; i < n; i++)
+    {
+        printf("a[%d]", i);
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+        printf("%d\t", a[i]);
+    }
+    printf("\n Sum=%d\t", sum);
+
+    return 0;
+}
